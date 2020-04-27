1c112569d3a53bdf63de5587f416482f96f066a7
Caleb Rudder <36672927+calebrudder@users.noreply.github.com>
2020-04-27 08:12:52 -0500

    Update database.php

--- database.php
+++ database.php
@@ -4,10 +4,10 @@
 function ConnectToDatabase()
 { 
 
-	// Your database name is the same as your username
+	
 	$mysqli = new mysqli("localhost", $db_username, $db_password, $db_database);
  
-	// Output error info if there was a connection problem
+	
 	if ($mysqli->connect_errno)
 		die("Failed to connect to MySQL: ($mysqli->connect_errno) $mysqli->connect_error");
 		
